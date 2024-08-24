05a674f42d9be28d223b6628faf28ff1b51c67f1
DenParvi <142200187+DenParvi@users.noreply.github.com>
2024-08-24 19:04:09 +0300

    Update roleScript.js

--- roleScript.js
+++ roleScript.js
@@ -19,7 +19,7 @@ const allRoles = [];
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    //[array[i], array[j]] = [array[j], array[i]];
   }
 }
 
